c4e710001d5d3fdd44539754b6c5e8d02696a484
vasudev8278 <vasudevsanchapu@gmail.com>
2025-01-22 11:01:20 +0530

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Homepage from './components/Homepage';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Homepage />
         <p>
           <code>WELCOME To BUSITRON </code>
         </p>
