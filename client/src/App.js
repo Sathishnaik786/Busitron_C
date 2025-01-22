68e20b00a194ceefc3c9fb7e05e26ff7676ba00a
pallavi678 <yanikipallipallavipallavi@gmail.com>
2025-01-22 15:05:10 +0530

    search bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import First from './Searchbar/First';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>WELCOME To BUSITRON </code>
-          <h1>this is pallavi</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://www.busitron.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Lets Start The Tasking
-        </a>
-      </header>
+      <section>
+        <First />
+      </section>
     </div>
   );
 }
