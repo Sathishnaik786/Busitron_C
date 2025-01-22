011d4a2bde1e00a18721028ae81ffe432dd75791
vasudev8278 <vasudevsanchapu@gmail.com>
2025-01-22 16:34:15 +0530

    auth

--- server/Server/index.js
+++ server/Server/index.js
@@ -1,14 +1,25 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const session = require('express-session');
+require('./config/passport');
+
+const authRoutes = require('./routes/authRoutes');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello, Node.js!');
-    res.send('Hello, Node.js!');
-    res.send('Hello, Node.js!');
-    res.send('Hello, Node.js!');res.send('Hello, Node.js!');res.send('Hello, Node.js!');
-});
+// Middleware
+app.use(express.json());
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+// Routes
+app.use('/auth', authRoutes);
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(5000, () => console.log('Server running on http://localhost:5000')))
+  .catch((err) => console.error(err));
