fdc8733a1c150c3f3270bf21717351ea9a1f224a
vasudev8278 <vasudevsanchapu@gmail.com>
2025-01-22 18:31:36 +0530

    commit

--- server/Server/index.js
+++ server/Server/index.js
@@ -3,23 +3,33 @@ const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const session = require('express-session');
-require('./config/passport');
+require('./config/passport'); // Configure Passport
 
 const authRoutes = require('./routes/authRoutes');
+const categoryRoutes = require('./routes/categoryRoutes');
 
 const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Routes
 app.use('/auth', authRoutes);
+app.use('/category', categoryRoutes);
 
 // MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(5000, () => console.log('Server running on http://localhost:5000')))
-  .catch((err) => console.error(err));
+  .then(() => {
+    app.listen(5000, () => console.log('Server running on http://localhost:5000'));
+  })
+  .catch((err) => console.error('MongoDB connection error:', err));
