f2912d48dc764d4382161dde88b32b5389471d7e
pallavi678 <yanikipallipallavipallavi@gmail.com>
2025-01-22 11:53:25 +0530

    change in p tag

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           <code>WELCOME To BUSITRON </code>
+          <h1>this is pallavi</h1>
         </p>
         <a
           className="App-link"
